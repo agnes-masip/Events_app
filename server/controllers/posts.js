3c70201cddb0736094d2b8c541e4d968bc6aea12
agnes.masip <agnes.masip@estudiantat.upc.edu>
2021-09-28 17:13:21 +0200

    environment variables

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -32,11 +32,13 @@ export const createPost = async (req,res) => {
 
 export const updatePost = async (req, res) => {
     const { id } = req.params;
-    const post = req.body;
+    const { title, message, creator, selectedFile, tags } = req.body;
     
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
     res.json(updatedPost);
 }
