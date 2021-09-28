1961a816cc943118d41d7abb80ff2fff7e186126
agnes.masip <agnes.masip@estudiantat.upc.edu>
2021-09-28 20:44:55 +0200

    ¨deploy2¨

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req,res) =>{
+    res.send('Hello to my events app');
+});
+
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
